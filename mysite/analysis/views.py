89808a334ce7a76da99539509ff850d2c6f3f0e0
wangweikang <weik.wang001@bkjk.com>
2019-06-14 17:45:17 +0800

    redis 持久化

--- mysite/analysis/views.py
+++ mysite/analysis/views.py
@@ -5,7 +5,7 @@ import redis
 from redis.sentinel import Sentinel
 
 sentinel = Sentinel([('sentinel', 26379)], socket_timeout=0.1)
-rds = sentinel.master_for('master', socket_timeout=0.1)
+rds = sentinel.master_for('redis-master', socket_timeout=0.1)
 
 def ping(request):
     rds.incr('count', 1)
